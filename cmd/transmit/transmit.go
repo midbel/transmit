ebd99d3e47ab23ac3ceff52fa8ab335fa7e35cd9
nico <brun.nicolas21@gmail.com>
2017-06-30 11:00:46 +0200

    rewrite transmit command

--- cmd/transmit/transmit.go
+++ cmd/transmit/transmit.go
@@ -1,204 +1,34 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"context"
-	"crypto/rand"
-	"crypto/tls"
-	"encoding/binary"
 	"encoding/json"
 	"errors"
 	"flag"
-	"hash/adler32"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
 	"os"
-	"os/signal"
-	"path/filepath"
-	"sync/atomic"
+	"sync"
 
-	_ "github.com/midbel/transmit"
-	"github.com/midbel/uuid"
+	"github.com/midbel/transmit"
 )
 
-var (
-	ErrUnknownId   = errors.New("packet with unknown packet id received")
-	ErrInvalidHash = errors.New("packet hash invalid")
-)
-
-type Conn struct {
-	net.Conn
-	Label []byte
-
-	count uint32
-	proxy bool
-	pad   io.Reader
-}
-
-func Dial(a string, id []byte, c *tls.Config) (net.Conn, error) {
-	w, err := net.Dial("tcp", a)
-	if w, ok := w.(*net.TCPConn); ok {
-		w.SetNoDelay(false)
-	}
-	if err != nil {
-		return nil, err
-	}
-	if c != nil {
-		w = tls.Client(w, c)
-	}
-	return &Conn{
-		Conn:  w,
-		Label: id,
-		pad:   bufio.NewReaderSize(rand.Reader, 4096),
-	}, nil
-}
-
-func (c *Conn) Write(d []byte) (int, error) {
-	b := new(bytes.Buffer)
-
-	b.Write(c.Label)
-	binary.Write(b, binary.BigEndian, uint16(len(d)))
-	binary.Write(b, binary.BigEndian, atomic.AddUint32(&c.count, 1))
-	b.Write(d)
-	if b.Len() < 512 {
-		io.CopyN(b, c.pad, 512)
-	}
-
-	_, err := io.Copy(c.Conn, b)
-	return len(d), err
-}
-
-func (c *Conn) Read(d []byte) (int, error) {
-	b := make([]byte, len(d))
-	if r, err := c.Conn.Read(b); err != nil {
-		return r, err
-	} else {
-		b = b[:r]
-	}
-	s := binary.BigEndian.Uint16(b[16:18])
-	if !bytes.Equal(b[:16], c.Label) {
-		return 0, ErrUnknownId
-	}
-	return copy(d, b[:22+s]), nil
-}
-
-type Group struct {
-	net.Conn
-	Label []byte
-}
-
-func (g *Group) Read(b []byte) (int, error) {
-	d := make([]byte, len(b))
-	r, err := g.Conn.Read(d)
-	if err != nil {
-		return r, err
-	}
-	sum := make([]byte, 4)
-	binary.BigEndian.PutUint32(sum, adler32.Checksum(d[:r]))
-
-	return copy(b, append(d[:r], sum...)), nil
-}
-
-func (g *Group) Write(b []byte) (int, error) {
-	data, sum := b[22:len(b)-adler32.Size], b[len(b)-adler32.Size:]
-	if a := adler32.Checksum(data); a != binary.BigEndian.Uint32(sum) {
-		return 0, ErrInvalidHash
-	}
-	_, err := g.Conn.Write(data)
-	return len(b), err
-}
-
-func (g *Group) UnmarshalJSON(b []byte) error {
-	v := struct {
-		Id     string `json:"id"`
-		Addr   string `json:"addr"`
-		Ifi    string `json:"interface"`
-		Listen bool   `json:"listen"`
-	}{}
-
-	var err error
-	if err = json.Unmarshal(b, &v); err != nil {
-		return err
-	}
-	u, err := uuid.UUID5(uuid.URL, []byte(v.Id))
-	if err != nil {
-		return err
-	}
-	g.Label = u.Bytes()
-	if v.Listen {
-		a, err := net.ResolveUDPAddr("udp", v.Addr)
-		if err != nil {
-			return err
-		}
-		g.Conn, err = net.ListenMulticastUDP("udp", nil, a)
-	} else {
-		g.Conn, err = net.Dial("udp", v.Addr)
-	}
-	return err
-}
+var ErrDone = errors.New("done")
 
 type Config struct {
-	Listen      bool   `json:"-"`
-	Address     string `json:"gateway"`
-	Verbose     bool   `json:"verbose"`
-	Size        int    `json:"size"`
-	Certificate *tls.Config
-
-	Proxy string `json:"proxy"`
-
-	Groups []*Group `json:"groups"`
-}
-
-func (c *Config) UnmarshalJSON(b []byte) error {
-	z := struct {
-		Location string `json:"location"`
-		Insecure bool   `json:"insecure"`
-		Server   string `json:"server"`
-	}{}
-	v := struct {
-		Address     string      `json:"gateway"`
-		Proxy       string      `json:"proxy"`
-		Verbose     bool        `json:"verbose"`
-		Size        int         `json:"size"`
-		Groups      []*Group    `json:"groups"`
-		Certificate interface{} `json:"certificate"`
-	}{Certificate: &z}
-	if err := json.Unmarshal(b, &v); err != nil {
-		return err
-	}
-	c.Address = v.Address
-	c.Verbose = v.Verbose
-	c.Proxy = v.Proxy
-	c.Size = v.Size
-	c.Groups = v.Groups
-
-	if i, err := os.Stat(z.Location); err == nil && i.IsDir() {
-		t := filepath.Join(z.Location, "transmit.cert")
-		k := filepath.Join(z.Location, "transmit.pem")
-
-		if cert, err := tls.LoadX509KeyPair(t, k); err != nil {
-			return err
-		} else {
-			c.Certificate = &tls.Config{
-				Certificates:       []tls.Certificate{cert},
-				ClientAuth:         tls.RequireAnyClientCert,
-				ServerName:         z.Server,
-				InsecureSkipVerify: z.Insecure,
-			}
-		}
-	}
-
-	return nil
+	Listen  bool             `json:"-"`
+	Verbose bool             `json:"-"`
+	Address string           `json:"gateway"`
+	Proxy   string           `json:"proxy"`
+	Padding int              `json:"padding"`
+	Routes  []transmit.Route `json:"routes"`
 }
 
 var config *Config
 
 func init() {
 	config = new(Config)
-
 	flag.BoolVar(&config.Listen, "l", config.Listen, "listen")
 	flag.BoolVar(&config.Verbose, "v", config.Verbose, "verbose")
 	flag.Parse()
@@ -207,144 +37,109 @@ func init() {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	defer f.Close()
-
-	if err := json.NewDecoder(f).Decode(config); err != nil {
-		log.Fatalln("bad configuration", err)
+	if err := json.NewDecoder(f).Decode(&config); err != nil {
+		log.Fatalln(err)
 	}
-	log.SetOutput(ioutil.Discard)
+	transmit.Padding = config.Padding
 }
 
 func main() {
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Kill, os.Interrupt)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		<-sig
-		cancel()
-		log.Println("done transmitting! abort on signal...")
-	}()
-	log.SetPrefix("transmit: ")
-	if config.Verbose {
-		log.SetOutput(os.Stderr)
-	}
-
 	var err error
 	switch {
 	case config.Listen:
-		err = distribute(ctx, *config)
+		err = distribute(config.Address, config.Proxy, config.Routes)
 	default:
-		err = forward(ctx, *config)
+		err = forward(config.Address, config.Routes)
 	}
 	if err != nil {
 		log.Fatalln(err)
 	}
 }
 
-func distribute(ctx context.Context, c Config) error {
-	s, err := net.Listen("tcp", c.Address)
+func distribute(a, p string, rs []transmit.Route) error {
+	r, err := transmit.NewRouter(a, rs)
 	if err != nil {
 		return err
 	}
-	go func() {
-		<-ctx.Done()
-		s.Close()
-	}()
-	if c.Certificate != nil {
-		s = tls.NewListener(s, c.Certificate)
-	}
-	log.Printf("listenning on %s", s.Addr())
-
+	var wg sync.WaitGroup
 	for {
-		r, err := s.Accept()
+		f, s, err := r.Accept()
 		if err != nil {
-			return err
-		}
-
-		id := make([]byte, 22)
-		if _, err := io.ReadFull(r, id); err != nil {
-			log.Printf("read transaction id from %s failed: %s", r.RemoteAddr(), err)
-			r.Close()
+			log.Println(err)
 			continue
 		}
-		id = id[:16]
-
-		var w, x net.Conn
-		for _, g := range c.Groups {
-			if bytes.Equal(id, g.Label) {
-				w = g
-				break
+		wg.Add(1)
+		go func(r, w net.Conn) {
+			var (
+				x net.Conn
+				i string
+			)
+			a := w.RemoteAddr()
+			for _, r := range rs {
+				if r.Addr == a.String() {
+					i = r.Id
+					break
+				}
 			}
-		}
-		if _, _, err := net.SplitHostPort(c.Proxy); err == nil {
-			var err error
-			if c.Certificate != nil {
-				x, err = tls.Dial("tcp", c.Proxy, c.Certificate)
-			} else {
-				x, err = net.Dial("tcp", c.Proxy)
+			if c, err := transmit.Forward(p, i); err == nil {
+				x = c
 			}
-			if _, err = x.Write(id); err != nil {
-				log.Printf("fail to initiate transaction with %s: %s", c.Proxy, err)
+			if err := relay(r, w, x); err != nil && err != ErrDone {
+				log.Println(err)
 			}
-			log.Printf("transaction initiated with %s: %x", c.Proxy, id)
-		}
-		if w == nil && x == nil {
-			log.Println("abort current transaction")
-			r.Close()
-			continue
-		}
-		log.Printf("transaction initiated: %x", id)
-		go transmit(ctx, &Conn{Conn: r, Label: id}, w, x)
+			wg.Done()
+		}(f, s)
 	}
+	wg.Wait()
 
 	return nil
 }
 
-func forward(ctx context.Context, c Config) error {
-	for _, g := range c.Groups {
-		w, err := Dial(config.Address, g.Label, c.Certificate)
+func forward(a string, rs []transmit.Route) error {
+	var wg sync.WaitGroup
+	for _, r := range rs {
+		f, err := transmit.Forward(a, r.Id)
 		if err != nil {
 			return err
 		}
-		log.Printf("transaction initiated: %x", g.Label)
-		if _, err := w.Write([]byte{}); err != nil {
+		s, err := transmit.Subscribe(r.Addr, r.Eth)
+		if err != nil {
 			return err
 		}
-		go transmit(ctx, g, w, nil)
+		wg.Add(1)
+		go func(r, w net.Conn) {
+			if err := relay(r, w, nil); err != nil && err != ErrDone {
+				log.Println(err)
+			}
+			wg.Done()
+		}(s, f)
 	}
-	<-ctx.Done()
-
+	wg.Wait()
 	return nil
 }
 
-func transmit(ctx context.Context, r, w, p net.Conn) {
-	go func() {
-		<-ctx.Done()
+func relay(r io.ReadCloser, w, x io.WriteCloser) error {
+	defer func() {
 		r.Close()
 		w.Close()
-		if p != nil {
-			p.Close()
+		if x != nil {
+			x.Close()
 		}
 	}()
-	var c io.Reader
-	if p != nil {
-		c = io.TeeReader(r, p)
-	} else {
-		c = r
+	if x != nil {
+		r = ioutil.NopCloser(io.TeeReader(r, x))
 	}
 	for {
-		_, err := io.Copy(w, c)
+		_, err := io.Copy(w, r)
 		switch err {
-		case io.EOF:
-			return
 		case nil:
+		case io.EOF:
+			return ErrDone
 		default:
-			if err, ok := err.(net.Error); ok && !(err.Timeout() || err.Temporary()) {
-				log.Printf("connection closed with remote host: %s", err)
-				return
+			if e, ok := err.(net.Error); !ok || !(e.Temporary() || e.Timeout()) {
+				return err
 			}
-			log.Printf("fail to transmit packet: %s", err)
 		}
 	}
+	return nil
 }
