f2f810e657c6afdb9767cec16da375eafd32833f
nico <brun.nicolas21@gmail.com>
2018-02-18 20:45:18 +0100

    gateway skeleton

--- cmd/transmit/gateway.go
+++ cmd/transmit/gateway.go
@@ -1,28 +1,56 @@
 package main
 
 import (
+	"crypto/tls"
 	"net"
+	"os"
 
 	"github.com/midbel/cli"
+	"github.com/midbel/toml"
 )
 
 var gateway = &cli.Command{
-	Usage: "gateway <config.toml>",
-	Alias: []string{"recv", "listen", "gw"},
 	Run:   runGateway,
+	Usage: "gateway <config.toml>",
+	Short: "",
+	Alias: []string{"recv", "listen", "gw"},
+	Desc:  ``,
 }
 
 func runGateway(cmd *cli.Command, args []string) error {
 	if err := cmd.Flag.Parse(args); err != nil {
 		return err
 	}
+	f, err := os.Open(cmd.Flag.Arg(0))
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	c := struct {
+		Addr string `toml:"address"`
+		Cert cert   `toml:"certificate"`
+	}{}
+	if err := toml.NewDecoder(f).Decode(&c); err != nil {
+		return err
+	}
+	queue, err := listen(c.Addr, c.Cert.Server())
+	if err != nil {
+		return err
+	}
+	for c := range queue {
+		_ = c
+	}
 	return nil
 }
 
-func listen(a, c string) (<-chan net.Conn, error) {
+func listen(a string, c *tls.Config) (<-chan net.Conn, error) {
 	s, err := net.Listen("tcp", a)
 	if err != nil {
 		return nil, err
+	}
+	if c != nil {
+		s = tls.NewListener(s, c)
 	}
 	q := make(chan net.Conn)
 	go func() {
