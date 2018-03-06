c418e5dcdef81252948b24bf36d81171454f70fe
nico <brun.nicolas21@gmail.com>
2018-03-06 12:40:35 +0100

    give packet to all configured routes from reader

--- cmd/transmit/gateway.go
+++ cmd/transmit/gateway.go
@@ -1,21 +1,20 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
+	"sort"
 
 	"github.com/midbel/cli"
 	"github.com/midbel/toml"
 	"github.com/midbel/transmit"
 )
-
-type Packet struct {
-	*transmit.Packet
-	net.Addr
-}
 
 var gateway = &cli.Command{
 	Run:   runGateway,
@@ -23,6 +22,67 @@ var gateway = &cli.Command{
 	Short: "",
 	Alias: []string{"recv", "listen", "gw"},
 	Desc:  ``,
+}
+
+type route struct {
+	Proto string   `toml:"proto"`
+	Addr  string   `toml:"address"`
+	Ports []uint16 `toml:"port"`
+}
+
+type conn struct {
+	addr  net.Addr
+	inner net.Conn
+
+	ports []uint16
+}
+
+func (c *conn) Transmit(p *transmit.Packet) error {
+	if len(c.ports) > 0 {
+		ix := sort.Search(len(c.ports), func(i int) bool {
+			return c.ports[i] <= p.Port
+		})
+		if ix >= len(c.ports) || c.ports[ix] != p.Port {
+			return fmt.Errorf("not found", c.ports, p.Port)
+		}
+	}
+	var err error
+	if c.inner == nil {
+		c.inner, err = net.Dial(c.addr.Network(), c.addr.String())
+		if err != nil {
+			return err
+		}
+	}
+	switch i := c.inner.(type) {
+	case *net.TCPConn:
+		_, err = i.Write(transmit.EncodePacket(p))
+	case *net.UDPConn:
+		_, err = i.Write(p.Payload)
+	}
+	if err != nil {
+		if err, ok := err.(net.Error); ok && !err.Temporary() {
+			c.inner.Close()
+			c.inner = nil
+		}
+	}
+	return err
+}
+
+type pool struct {
+	cs []*conn
+}
+
+func (p *pool) Write(bs []byte) (int, error) {
+	r := bufio.NewReader(bytes.NewReader(bs))
+	k, err := transmit.DecodePacket(r)
+	if err == nil {
+		for _, c := range p.cs {
+			if err := c.Transmit(k); err != nil {
+				log.Println(err)
+			}
+		}
+	}
+	return len(bs), nil
 }
 
 func runGateway(cmd *cli.Command, args []string) error {
@@ -36,19 +96,52 @@ func runGateway(cmd *cli.Command, args []string) error {
 	defer f.Close()
 
 	c := struct {
-		Addr string `toml:"address"`
+		Addr   string  `toml:"address"`
+		Debug  bool    `toml:"dump"`
+		Routes []route `toml:"route"`
 	}{}
 	if err := toml.NewDecoder(f).Decode(&c); err != nil {
+		return err
+	}
+	w, err := prepare(c.Routes, c.Debug)
+	if err != nil {
 		return err
 	}
 	r, err := listen(c.Addr)
 	if err != nil {
 		return err
 	}
-	w := hex.Dumper(os.Stdout)
 	_, err = io.Copy(w, r)
 
 	return nil
+}
+
+func prepare(rs []route, d bool) (io.Writer, error) {
+	p := new(pool)
+	for _, r := range rs {
+		var (
+			a   net.Addr
+			err error
+		)
+		switch r.Proto {
+		case "", "udp":
+			a, err = net.ResolveUDPAddr("udp", r.Addr)
+		case "tcp":
+			a, err = net.ResolveTCPAddr("tcp", r.Addr)
+		default:
+			return nil, fmt.Errorf("unsupported protocol: %s", r.Proto)
+		}
+		if err != nil {
+			return nil, err
+		}
+		p.cs = append(p.cs, &conn{addr: a})
+	}
+	var w io.Writer = p
+	if d {
+		w = io.MultiWriter(p, hex.Dumper(os.Stdout))
+	}
+
+	return w, nil
 }
 
 func listen(a string) (io.Reader, error) {
