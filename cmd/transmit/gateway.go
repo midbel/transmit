18e79bad6018b3e6d3219bcc251d1f07da774be2
nico <brun.nicolas21@gmail.com>
2018-03-06 18:03:51 +0100

    cleaning

--- cmd/transmit/gateway.go
+++ cmd/transmit/gateway.go
@@ -1,20 +1,15 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"crypto/tls"
 	"encoding/hex"
-	"fmt"
 	"io"
 	"log"
 	"net"
 	"os"
-	"sort"
 
 	"github.com/midbel/cli"
 	"github.com/midbel/toml"
-	"github.com/midbel/transmit"
 )
 
 var gateway = &cli.Command{
@@ -29,74 +24,6 @@ type route struct {
 	Proto string   `toml:"proto"`
 	Addr  string   `toml:"address"`
 	Ports []uint16 `toml:"port"`
-}
-
-type conn struct {
-	addr  net.Addr
-	inner net.Conn
-
-	ports []uint16
-}
-
-func (c *conn) Transmit(p *transmit.Packet) error {
-	if len(c.ports) > 0 {
-		ix := sort.Search(len(c.ports), func(i int) bool {
-			return c.ports[i] <= p.Port
-		})
-		if ix >= len(c.ports) || c.ports[ix] != p.Port {
-			return fmt.Errorf("not found", c.ports, p.Port)
-		}
-	}
-	var err error
-	if c.inner == nil {
-		c.inner, err = net.Dial(c.addr.Network(), c.addr.String())
-		if err != nil {
-			return err
-		}
-	}
-	switch i := c.inner.(type) {
-	case *net.TCPConn:
-		_, err = i.Write(transmit.EncodePacket(p))
-	case *net.UDPConn:
-		_, err = i.Write(p.Payload)
-	}
-	if err != nil {
-		if err, ok := err.(net.Error); ok && !err.Temporary() {
-			c.inner.Close()
-			c.inner = nil
-		}
-	}
-	return err
-}
-
-type pool struct {
-	cs []*conn
-}
-
-func (p *pool) Close() error {
-	var err error
-	for _, c := range p.cs {
-		if c.inner == nil {
-			continue
-		}
-		if e := c.inner.Close(); e != nil {
-			err = e
-		}
-	}
-	return err
-}
-
-func (p *pool) Write(bs []byte) (int, error) {
-	r := bufio.NewReader(bytes.NewReader(bs))
-	k, err := transmit.DecodePacket(r)
-	if err == nil {
-		for _, c := range p.cs {
-			if err := c.Transmit(k); err != nil {
-				log.Println(err)
-			}
-		}
-	}
-	return len(bs), nil
 }
 
 func runGateway(cmd *cli.Command, args []string) error {
@@ -120,50 +47,18 @@ func runGateway(cmd *cli.Command, args []string) error {
 	if err := toml.NewDecoder(f).Decode(&c); err != nil {
 		return err
 	}
-	w, err := prepare(c.Routes, c.Debug)
-	if err != nil {
-		return err
-	}
 
 	r, err := listen(c.Addr, c.Cert.Server())
 	if err != nil {
 		return err
 	}
+	var b []byte
 	if c.Buffer > 0 {
-		b := make([]byte, c.Buffer)
-		_, err = io.CopyBuffer(w, r, b)
-	} else {
-		_, err = io.Copy(w, r)
+		b = make([]byte, c.Buffer)
 	}
+	w := hex.Dumper(os.Stdout)
+	_, err = io.CopyBuffer(w, r, b)
 	return nil
-}
-
-func prepare(rs []route, d bool) (io.Writer, error) {
-	p := new(pool)
-	for _, r := range rs {
-		var (
-			a   net.Addr
-			err error
-		)
-		switch r.Proto {
-		case "", "udp":
-			a, err = net.ResolveUDPAddr("udp", r.Addr)
-		case "tcp":
-			a, err = net.ResolveTCPAddr("tcp", r.Addr)
-		default:
-			return nil, fmt.Errorf("unsupported protocol: %s", r.Proto)
-		}
-		if err != nil {
-			return nil, err
-		}
-		p.cs = append(p.cs, &conn{addr: a})
-	}
-	var w io.Writer = p
-	if d {
-		w = io.MultiWriter(p, hex.Dumper(os.Stdout))
-	}
-
-	return w, nil
 }
 
 func listen(a string, c *tls.Config) (io.Reader, error) {
