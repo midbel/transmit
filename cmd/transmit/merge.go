b99277be3eb4576c3a77c7abb120ce582ca79ed2
nico <brun.nicolas21@gmail.com>
2018-04-05 15:00:37 +0200

    merge and foward final

--- cmd/transmit/merge.go
+++ cmd/transmit/merge.go
@@ -2,19 +2,16 @@ package main
 
 import (
 	"bytes"
-	"container/heap"
 	"crypto/md5"
 	"encoding/binary"
 	"errors"
 	"fmt"
-	"hash"
 	"hash/adler32"
 	"io"
-	"log"
+	// "log"
 	"net"
 	"sort"
 	"sync"
-	"time"
 
 	"github.com/midbel/cli"
 )
@@ -24,71 +21,6 @@ var (
 	ErrRollSum  = errors.New("rolling checksum mismatched")
 	ErrChecksum = errors.New("md5 checksum mismatched")
 )
-
-type ChunkQueue []*Chunk
-
-func (q *ChunkQueue) Push(v interface{}) {
-	c, ok := v.(*Chunk)
-	if !ok {
-		return
-	}
-	*q = append(*q, c)
-}
-
-func (q *ChunkQueue) Pop() interface{} {
-	if len(*q) == 0 {
-		return nil
-	}
-	cs := *q
-	c := cs[len(cs)-1]
-	*q = cs[:len(cs)-1]
-
-	return c
-}
-
-func (q ChunkQueue) Len() int {
-	return len(q)
-}
-
-func (q ChunkQueue) Less(i, j int) bool {
-	return q[i].Id <= q[j].Id
-}
-
-func (q ChunkQueue) Swap(i, j int) {
-	q[i], q[j] = q[j], q[i]
-}
-
-type Buffer struct {
-	next   uint32
-	chunks ChunkQueue
-}
-
-func (b *Buffer) Pop() *Chunk {
-	if b.chunks.Len() == 0 {
-		return nil
-	}
-	x := heap.Pop(&b.chunks)
-	if x == nil {
-		return nil
-	}
-	c := x.(*Chunk)
-	if c.Id == b.next {
-		b.next = c.Id + 1
-		return c
-	}
-	heap.Push(&b.chunks, c)
-	return nil
-}
-
-func (b *Buffer) Push(c *Chunk) {
-	heap.Push(&b.chunks, c)
-}
-
-func NewBuffer() *Buffer {
-	q := make(ChunkQueue, 0, 100)
-	heap.Init(&q)
-	return &Buffer{next: 1, chunks: q}
-}
 
 type Chunk struct {
 	Key
@@ -128,145 +60,128 @@ func (k Key) Equal(s []byte) bool {
 	return bytes.Equal(s, k.Sum[:])
 }
 
-// type merger struct {
-// 	mu     sync.Mutex
-// 	chunks map[Key][]*Chunk
-//
-// 	errors chan error
-// 	blocks chan *Block
-// }
-//
-// func Merge(a string) (*merger, error) {
-// 	c, err := net.Listen("tcp", a)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	m := &merger{}
-// 	return m, nil
-// }
-//
-// func (m *merger) Next() (*Block, error) {
-// 	select {
-// 	case b, ok := <-m.blocks:
-// 		if !ok {
-// 			break
-// 		}
-// 		return b, nil
-// 	case e, ok := <-m.errors:
-// 		if !ok {
-// 			break
-// 		}
-// 		return nil, e
-// 	}
-// 	return nil, ErrDone
-// }
-//
-// func (m *merger) build(k Key, cs []*Chunk) {
-// 	buf := new(bytes.Buffer)
-// 	roll, sum := adler32.New(), md5.New()
-// 	w := io.MultiWriter(buf, sum, roll)
-//
-// 	sort.Slice(cs, func(i, j int) bool {
-// 		return cs[i].Frag <= cs[j].Frag
-// 	})
-// 	for _, i := range cs {
-// 		w.Write(i.Payload)
-// 		if s := roll.Sum32(); s != i.Roll {
-// 			// return nil, ErrRollSum
-// 			return
-// 		}
-// 	}
-// 	if s := sum.Sum(nil); !k.Equal(s) {
-// 		// return nil, ErrChecksum
-// 		return
-// 	}
-// 	b := &Block{
-// 		Id:      k.Id,
-// 		Port:    k.Dst,
-// 		Sum:     k.Sum[:],
-// 		Payload: make([]byte, buf.Len()),
-// 	}
-// 	if _, err := io.ReadFull(buf, b.Payload); err != nil {
-// 		return
-// 	}
-// }
-//
-// func (m *merger) merge(c *Chunk) {
-// 	m.mu.Lock()
-// 	defer m.mu.Unlock()
-//
-// 	cs := append(m.chunks[c.Key], c)
-// 	if len(cs) >= int(c.Count) {
-// 		delete(m.chunks, c.Key)
-// 		go m.build(c.Key, cs)
-// 	} else {
-// 		m.chunks[c.Key] = cs
-// 	}
-// }
-
 type merger struct {
+	inner net.Listener
+
 	mu     sync.Mutex
 	chunks map[Key][]*Chunk
-	when   map[Key]time.Time
 
-	sum  hash.Hash
-	roll hash.Hash32
+	errors chan error
+	blocks chan *Block
 }
 
-func Merge() *merger {
-	return &merger{
-		chunks: make(map[Key][]*Chunk),
-		when:   make(map[Key]time.Time),
-		sum:    md5.New(),
-		roll:   adler32.New(),
+func Merge(a string) (*merger, error) {
+	c, err := net.Listen("tcp", a)
+	if err != nil {
+		return nil, err
 	}
+	m := &merger{
+		inner:  c,
+		chunks: make(map[Key][]*Chunk),
+		errors: make(chan error),
+		blocks: make(chan *Block),
+	}
+	go m.listen()
+	return m, nil
 }
 
-func (m *merger) Merge(c *Chunk) (*Chunk, error) {
+func (m *merger) Next() (*Block, error) {
+	select {
+	case b, ok := <-m.blocks:
+		if !ok {
+			break
+		}
+		return b, nil
+	case e, ok := <-m.errors:
+		if !ok {
+			break
+		}
+		return nil, e
+	}
+	return nil, ErrDone
+}
+
+func (m *merger) build(k Key, cs []*Chunk) {
+	buf := new(bytes.Buffer)
+	roll, sum := adler32.New(), md5.New()
+	w := io.MultiWriter(buf, sum, roll)
+
+	sort.Slice(cs, func(i, j int) bool {
+		return cs[i].Frag <= cs[j].Frag
+	})
+	for _, i := range cs {
+		w.Write(i.Payload)
+		if s := roll.Sum32(); s != i.Roll {
+			m.errors <- ErrRollSum
+			// return nil, ErrRollSum
+			return
+		}
+	}
+	if s := sum.Sum(nil); !k.Equal(s) {
+		m.errors <- ErrChecksum
+		// return nil, ErrChecksum
+		return
+	}
+	b := &Block{
+		Id:      k.Id,
+		Port:    k.Dst,
+		Sum:     k.Sum[:],
+		Addr:    &net.TCPAddr{IP: k.IP[:], Port: int(k.Src)},
+		Payload: make([]byte, buf.Len()),
+	}
+	if _, err := io.ReadFull(buf, b.Payload); err != nil {
+		m.errors <- err
+		return
+	}
+	m.blocks <- b
+}
+
+func (m *merger) merge(c *Chunk) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 
 	cs := append(m.chunks[c.Key], c)
 	if len(cs) >= int(c.Count) {
 		delete(m.chunks, c.Key)
-		delete(m.when, c.Key)
-
-		sort.Slice(cs, func(i, j int) bool {
-			return cs[i].Frag <= cs[j].Frag
-		})
-		m.roll.Reset()
-		m.sum.Reset()
-
-		buf := new(bytes.Buffer)
-		w := io.MultiWriter(buf, m.sum, m.roll)
-
-		for _, i := range cs {
-			w.Write(i.Payload)
-			if s := m.roll.Sum32(); s != i.Roll {
-				return nil, ErrRollSum
-			}
-		}
-		c.Roll = cs[len(cs)-1].Roll
-		if s := m.sum.Sum(nil); !c.Key.Equal(s) {
-			return nil, ErrChecksum
-		}
-		c.Length, c.Frag = uint16(buf.Len()), 0
-		c.Payload = buf.Bytes()
-		return c, nil
+		go m.build(c.Key, cs)
+	} else {
+		m.chunks[c.Key] = cs
 	}
-	m.when[c.Key], m.chunks[c.Key] = time.Now(), cs
-	return nil, nil
+}
+
+func (m *merger) listen() {
+	for {
+		c, err := m.inner.Accept()
+		if err != nil {
+			return
+		}
+		if c, ok := c.(*net.TCPConn); ok {
+			c.SetKeepAlive(true)
+		}
+		go func(r net.Conn) {
+			defer r.Close()
+			for {
+				c, err := decodeChunk(r)
+				if err != nil {
+					return
+				}
+				m.merge(c)
+			}
+		}(c)
+	}
 }
 
 type forwarder struct {
-	addrs map[uint16]net.Addr
-	conns map[net.Addr]net.Conn
+	addrs  map[uint16]net.Addr
+	conns  map[addr]net.Conn
+	blocks map[addr]*BlockList
 }
 
 func Forward(as ...string) (*forwarder, error) {
 	f := &forwarder{
-		addrs: make(map[uint16]net.Addr),
-		conns: make(map[net.Addr]net.Conn),
+		addrs:  make(map[uint16]net.Addr),
+		conns:  make(map[addr]net.Conn),
+		blocks: make(map[addr]*BlockList),
 	}
 	for _, a := range as {
 		a, err := net.ResolveTCPAddr("tcp", a)
@@ -283,145 +198,59 @@ func (f *forwarder) Forward(b *Block) error {
 	if !ok {
 		return fmt.Errorf("no route configued to %d", b.Port)
 	}
+	s := b.Stream()
+	q, ok := f.blocks[s]
+	if !ok {
+		q = List()
+		f.blocks[s] = q
+	}
+	q.Push(b)
+
 	var err error
-	c, ok := f.conns[a]
+	c, ok := f.conns[s]
 	if !ok {
 		c, err = net.Dial(a.Network(), a.String())
 		if err != nil {
 			return err
 		}
-		f.conns[a] = c
+		f.conns[s] = c
 	}
-	_, err = c.Write(b.Payload)
-	return err
-}
-
-// TODO: make it as runMerge (shorter than current)
-// func runMergeBis(cmd *cli.Command, args []string) error {
-// 	if err := cmd.Flag.Parse(args); err != nil {
-// 		return err
-// 	}
-// 	addrs := cmd.Flag.Args()
-// 	f, err := Forward(addrs[1:]...)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer f.Close()
-// 	m, err := Merge(cmd.Flag.Arg(0))
-// 	if err != nil {
-// 		return err
-// 	}
-// 	for {
-// 		switch b, err := m.Next(); err {
-// 		case nil:
-// 			if err := f.Forward(b); err != nil {
-// 				return err
-// 			}
-// 		case ErrDone:
-// 			return nil
-// 		default:
-// 			return err
-// 		}
-// 	}
-// 	return nil
-// }
-
-func runMerge(cmd *cli.Command, args []string) error {
-	discard := cmd.Flag.Bool("d", false, "discard")
-	if err := cmd.Flag.Parse(args); err != nil {
-		return err
-	}
-	ws := make(map[uint16]net.Addr)
-	cs := make(map[interface{}]net.Conn)
-	for i := 1; i < cmd.Flag.NArg(); i++ {
-		a, err := net.ResolveTCPAddr("tcp", cmd.Flag.Arg(i))
+	for b := q.Pop(); b != nil; b = q.Pop() {
+		_, err = c.Write(b.Payload)
 		if err != nil {
 			return err
-		}
-		ws[uint16(a.Port)] = a
-	}
-	queue, err := listenAndMerge(cmd.Flag.Arg(0))
-	if err != nil {
-		return err
-	}
-	m := Merge()
-	when := make(map[Key]time.Time)
-	dtstamp := time.Now()
-
-	pq := NewBuffer()
-	for c := range queue {
-		if _, ok := when[c.Key]; !ok {
-			when[c.Key] = time.Now()
-		}
-		k, err := m.Merge(c)
-		if err != nil {
-			continue
-		}
-		if k == nil {
-			continue
-		}
-		pq.Push(k)
-		if k = pq.Pop(); k == nil {
-			continue
-		}
-		log.Printf("%6d | %6d | %9d | %x | %16s | %16s", k.Id, k.Count, len(c.Payload), k.Sum, time.Since(when[c.Key]), time.Since(dtstamp))
-		delete(when, k.Key)
-		if a, ok := ws[k.Key.Dst]; ok && !*discard {
-			n := k.Route()
-			w, ok := cs[n]
-			if !ok {
-				c, err := net.Dial(a.Network(), a.String())
-				if err != nil {
-					continue
-				}
-				cs[n] = c
-				w = c
-			}
-			if _, err := w.Write(k.Payload); err != nil {
-				w.Close()
-				delete(cs, n)
-			}
 		}
 	}
 	return nil
 }
 
-func listenAndMerge(a string) (<-chan *Chunk, error) {
-	s, err := net.Listen("tcp", a)
-	if err != nil {
-		return nil, err
+func runMerge(cmd *cli.Command, args []string) error {
+	if err := cmd.Flag.Parse(args); err != nil {
+		return err
 	}
-
-	queue := make(chan *Chunk)
-	go func() {
-		defer func() {
-			close(queue)
-			s.Close()
-		}()
-		for {
-			c, err := s.Accept()
-			if err != nil {
-				return
+	addrs := cmd.Flag.Args()
+	f, err := Forward(addrs[1:]...)
+	if err != nil {
+		return err
+	}
+	// defer f.Close()
+	m, err := Merge(cmd.Flag.Arg(0))
+	if err != nil {
+		return err
+	}
+	for {
+		switch b, err := m.Next(); err {
+		case nil:
+			if err := f.Forward(b); err != nil {
+				return err
 			}
-			if c, ok := c.(*net.TCPConn); ok {
-				c.SetKeepAlive(true)
-			}
-			go func(c net.Conn) {
-				defer c.Close()
-				for {
-					c, err := decodeChunk(c)
-					if err, ok := err.(net.Error); ok && !err.Temporary() {
-						return
-					}
-					if err != nil {
-						return
-					}
-					queue <- c
-				}
-			}(c)
+		case ErrDone:
+			return nil
+		default:
+			return err
 		}
-	}()
-	return queue, nil
+	}
+	return nil
 }
 
 func decodeChunk(r io.Reader) (*Chunk, error) {
